ca3cee19a4fe54c500e73d9ee3c17d48ff073c70
JayaChandraTuthika <chandra4204@gmail.com>
2023-02-24 18:12:44 +0000

    React-context-sessions

---
+++ react-context-part-4/src/components/Cart/index.js
@@ -0,0 +1,31 @@
+import CartContext from '../../context/CartContext'
+import Header from '../Header'
+import CartListView from '../CartListView'
+import EmptyCartView from '../EmptyCartView'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      const showEmptView = cartList.length === 0
+      return (
+        <>
+          <Header />
+          {showEmptView ? (
+            <EmptyCartView />
+          ) : (
+            <div className="cart-container">
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <CartListView />
+              </div>
+            </div>
+          )}
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
